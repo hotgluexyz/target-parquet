f298a0c60ce7c5ca95912fc8da8a2d403da8e9b1
Key <keynarafaelp@gmail.com>
2026-08-13 14:48:49 -0500

    delete log

--- target_parquet/sinks.py
+++ target_parquet/sinks.py
@@ -157,7 +157,6 @@ class ParquetSink(BatchSink):
 
     @property
     def max_size(self) -> int:
-        self.logger.info(f"PARQUET_BATCH_MAX_SIZE: {os.getenv('PARQUET_BATCH_MAX_SIZE')}")
         env_value = os.getenv("PARQUET_BATCH_MAX_SIZE")
         if env_value is not None and env_value != "":
             try:
